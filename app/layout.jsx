920ee6d59709d20a347e69f1f09c5d4a2e7f6254
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2023-09-26 19:31:26 +0500

    - general app layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,20 @@
-export default function Layout() {
-  return <div>Layout</div>;
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptai",
+  description: "Discover & Share AI Prompts",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
 }
