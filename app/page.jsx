ac486782f1baef5e0e7b3369a18eb797b13217cd
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2023-09-27 18:06:39 +0500

    - Created route for Feed, Form and Profile - Created Navigation bar for desktop and mobile views

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
+import Feed from "@components/Feed";
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="md:max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptai is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+
+      <Feed />
+    </section>
+  );
 }
